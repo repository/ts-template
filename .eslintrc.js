778d0985c24c4dc18c4d2276475576ea51d9f8cc
stanley <41586666+repository@users.noreply.github.com>
2021-04-27 14:20:09 -0700

    init

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint/eslint-plugin", "simple-import-sort"],
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: ["node_modules/*", "dist/*", "!.prettierrc.js"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  },
+};
