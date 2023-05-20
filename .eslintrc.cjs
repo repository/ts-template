a985fec9b9cc1a318ba5c1839f05fed0027707de
repository <xcvr48@gmail.com>
2023-05-19 18:42:29 -0700

    update

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended", "prettier"],
+  root: true,
+};
